9b60e08e35b751b3cee7265276fd94cd2b4873f3
G Roques <g.roques@charter.com>
2023-04-12 14:42:46 -0500

    Add extended client capabilities, gd, & comments

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,9 +1,11 @@
 -- Eclipse Java development tools (JDT) Language Server downloaded from:
 -- https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.21.0/jdt-language-server-1.21.0-202303161431.tar.gz
+local jdtls = require('jdtls')
 local jdtls_path = '~/jdt-language-server'
 local launcher_jar = vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
 local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
     cmd = {
         -- java MUST be in PATH and version 17 or greater.
@@ -22,5 +24,9 @@ local config = {
         "-data", vim.fn.expand('~/.cache/jdtls-workspace/') .. workspace_dir
     },
     root_dir = vim.fs.dirname(vim.fs.find({'pom.xml', '.git'}, { upward = true })[1]),
+    init_options = {
+        -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Language-Server-Settings-&-Capabilities#extended-client-capabilities
+        extendedClientCapabilities = jdtls.extendedClientCapabilities,
+    },
 }
-require('jdtls').start_or_attach(config)
+jdtls.start_or_attach(config)
--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- General
+-- -------
 vim.wo.relativenumber = true
 
+-- Plugins
+-- -------
 require('plugins')
+
+-- Language Server Protocol
+-- ------------------------
+-- https://github.com/neovim/nvim-lspconfig/blob/master/README.md#suggested-configuration
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
