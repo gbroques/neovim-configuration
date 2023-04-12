40ed3e3f5740084fd6bfa34888989cc0de5a5390
G Roques <g.roques@charter.com>
2023-04-12 18:39:25 -0500

    Add onedark.nvim theme

--- init.lua
+++ init.lua
@@ -1,10 +1,39 @@
 -- General
 -- -------
+-- Set relative line numbers.
 vim.wo.relativenumber = true
+-- Highlight cursor line.
+vim.o.cursorline = true
 
 -- Plugins
 -- -------
 require('plugins')
+
+-- Theme
+-- -----
+require('onedark').setup  {
+    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = true, -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    code_style = {
+        comments = 'none'
+    },
+    colors = {
+        -- Override grey with tint for lighter line numbers.
+        -- https://github.com/navarasu/onedark.nvim/blob/932a86846d51f3d39a99f7e4d0c0ec34d4e404d3/lua/onedark/palette.lua#L75
+        -- https://www.color-hex.com/color/546178
+        grey = "#8790A0",
+        -- Override bg1 with shade for darker cursor line.
+        -- https://github.com/navarasu/onedark.nvim/blob/932a86846d51f3d39a99f7e4d0c0ec34d4e404d3/lua/onedark/palette.lua#L61
+        -- https://www.color-hex.com/color/2d3343
+        bg1 = '#242835',
+        -- Override foreground with tint.
+        -- https://github.com/navarasu/onedark.nvim/blob/932a86846d51f3d39a99f7e4d0c0ec34d4e404d3/lua/onedark/palette.lua#L67
+        -- https://www.color-hex.com/color/a5b0c5
+        fg = '#b7bfd0'
+    }
+}
+require('onedark').load()
 
 -- Language Server Protocol
 -- ------------------------
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,5 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'mfussenegger/nvim-jdtls'
+    use 'navarasu/onedark.nvim'
 end)
