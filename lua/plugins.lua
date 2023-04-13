f71b51d14e441141dad6a57bd82f66f53ad449b8
G Roques <g.roques@charter.com>
2023-04-13 09:30:16 -0500

    Add vim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,5 +11,6 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use {'nvim-telescope/telescope-ui-select.nvim' }
+    use 'nvim-telescope/telescope-ui-select.nvim'
+    use 'tpope/vim-surround'
 end)
