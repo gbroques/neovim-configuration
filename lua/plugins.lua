5581f9d8a8a0b716317496039f87dc6e937974da
G Roques <g.roques@charter.com>
2023-04-14 20:37:20 -0500

    Add packer compile autocommand

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,6 +11,14 @@ end
 
 local packer_bootstrap = ensure_packer()
 
+-- Automatically run :PackerCompile whenever this file is saved.
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
     -- Package Manager
     use 'wbthomason/packer.nvim'
