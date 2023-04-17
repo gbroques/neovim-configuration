0cacd4262b5bca10488d586c27366d94a912c42a
G Roques <groques360@gmail.com>
2023-04-16 22:05:17 -0500

    Refactor completion into its own module

--- init.lua
+++ init.lua
@@ -4,36 +4,7 @@ require('options')
 require('plugins')
 require('user.telescope')
 require('user.autopairs')
-
--- completion
-local cmp = require('cmp')
-
--- snippet engine required for JDTLS completions
-local luasnip = require 'luasnip'
-luasnip.config.setup {}
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'nvim_lua' },
-    { name = 'buffer',  option = { keyword_length = 5 } }, -- increase from default of 3
-    { name = 'path' }
-  }),
-  -- TODO: Should this be in keymaps?
-  mapping = {
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
-})
+require('user.completion')
 
 -- theme
 -- -----
