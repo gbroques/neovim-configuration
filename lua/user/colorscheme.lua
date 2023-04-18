cafc08d67e7b13449e0742e39eadd7ec61b4d52b
G Roques <g.roques@charter.com>
2023-04-18 13:34:42 -0500

    Tweak colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,10 +5,17 @@ require("tokyonight").setup({
     functions = {},
     variables = {},
   },
+  -- test
   on_colors = function(colors)
     colors.comment = colors.dark5
+    colors.fg = '#d3dcf7'
+    colors.bg = "#1a1b26"
+    colors.bg_dark = "#16161e"
+  end,
+  on_highlights = function(hl, c)
+    hl.LineNr = { fg = c.fg_dark }
+    hl.CursorLineNr = { fg = c.fg }
   end
 })
 
-vim.cmd("colorscheme tokyonight-night")
-
+vim.cmd("colorscheme tokyonight-moon")
