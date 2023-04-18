aa2c974c330c7b03ae834c8898ab6737bb387d5a
G Roques <g.roques@charter.com>
2023-04-17 21:57:58 -0500

    Add helpful keymaps
    Better window / buffer management
    
    Clear highlights
    
    and more!
    
    https://github.com/LunarVim/nvim-basic-ide/blob/master/lua/keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,7 +12,32 @@ vim.g.mapleader = ' '
 -- 'dh' is a convenient escape from Insert mode in Colemak.
 require("better_escape").setup { mapping = { "dh" } }
 
+-- Normal --
+
+-- Save
 vim.keymap.set('n', '<leader>s', ':update<CR>', { desc = '(s)ave' })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Resize with arrows
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+
+-- Clear highlights
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
+
+-- Close buffers
+vim.keymap.set("n", "<S-q>", "<cmd>bdelete!<CR>")
 
 -- Telescope
 local builtin = require('telescope.builtin')
@@ -39,3 +64,9 @@ end)
 
 -- NvimTree
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>', { desc = '(e)xplorer' })
+
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
