2f9c7c861d7147df8586afa345d36912e41f9645
G Roques <g.roques@charter.com>
2023-04-17 23:20:31 -0500

    Tweak status & tab line

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -1,11 +1,3 @@
-local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
-end
-local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
--- lualine
 require("lualine").setup {
   options = {
     globalstatus = true,
@@ -16,25 +8,37 @@ require("lualine").setup {
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch" },
-    lualine_c = { {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      sections = { "error", "warn" },
-      symbols = { error = " ", warn = " " },
-      colored = true,
-      always_visible = true,
-    } },
-    lualine_x = { {
-      "diff",
-      colored = false,
-      symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-      cond = hide_in_width,
-    }, spaces, "encoding", {
-      "filetype",
-      icons_enabled = false,
-    } },
-    lualine_y = { { "location", padding = 1 } },
+    lualine_b = {
+      {
+        "filename",
+        path = 3,                -- 3: Absolute path, with tilde as the home directory
+        shorting_target = 10,    -- Shortens path to leave 10 spaces in the window
+        symbols = {
+          modified = '●',      -- Text to show when the file is modified.
+          readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]',     -- Text to show for newly created file before first write
+        }
+      },
+    },
+    lualine_c = {
+      {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        sections = { "error", "warn" },
+        symbols = { error = " ", warn = " " },
+        colored = true,
+        always_visible = false,
+      }
+    },
+    lualine_x = { "branch" },
+    lualine_y = {
+      {
+        "filetype",
+        icon_only = true,
+      },
+      { "location", padding = 1 },
+    },
     lualine_z = { "progress" },
   },
 }
--- lua/user/tabline.lua
+++ lua/user/tabline.lua
@@ -1,7 +1,8 @@
-
 require("bufferline").setup {
   options = {
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    show_buffer_close_icons = false,
+    separator_style = "thin"
   },
   highlights = { buffer_selected = { italic = false } }
 }
