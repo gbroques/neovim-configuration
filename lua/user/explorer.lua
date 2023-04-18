79e3cd81fe037b9fd4d001874482393b02e00ddb
G Roques <g.roques@charter.com>
2023-04-17 22:15:31 -0500

    Change nvim-tree icons & enable diagnostics on dirs

--- lua/user/explorer.lua
+++ lua/user/explorer.lua
@@ -1,6 +1,45 @@
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require('nvim-tree').setup {
   renderer = {
     -- Group empty directories for Java development
     group_empty = true,
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
   },
 }
