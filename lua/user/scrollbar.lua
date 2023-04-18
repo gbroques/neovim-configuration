fb0a1ff362eef783ea045c8f080cf1b56c99832b
G Roques <g.roques@charter.com>
2023-04-17 23:45:41 -0500

    Add todo for scrollbar

--- lua/user/scrollbar.lua
+++ lua/user/scrollbar.lua
@@ -1,6 +1,8 @@
+-- TODO: Investigate theme / colorscheme support.
+--       Does darkplus support scrollbar?
+--       Should we switch to folke/tokyonight.nvim?
 require("scrollbar").setup({
   handlers = {
     gitsigns = true,     -- Requires gitsigns
-    search = false,      -- Requires hlslens
   },
 })
