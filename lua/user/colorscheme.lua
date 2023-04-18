7bbcd404a4fd310d42b67b5bc767c6ea72d86c1b
G Roques <g.roques@charter.com>
2023-04-18 13:09:42 -0500

    Disable italics and brighten comment color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,2 +1,14 @@
+require("tokyonight").setup({
+  styles = {
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = {},
+    variables = {},
+  },
+  on_colors = function(colors)
+    colors.comment = colors.dark5
+  end
+})
+
 vim.cmd("colorscheme tokyonight-night")
 
