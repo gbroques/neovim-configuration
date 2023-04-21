45834ffed6233449f1e1efa62b206b74886b0f1b
G Roques <groques360@gmail.com>
2023-04-20 21:34:30 -0500

    Toggle show_first_indent_level to true

--- lua/user/indent-guide.lua
+++ lua/user/indent-guide.lua
@@ -1,6 +1,6 @@
 require("indent_blankline").setup {
   show_trailing_blankline_indent = false,
-  show_first_indent_level = false,
+  show_first_indent_level = true,
   use_treesitter = true,
   show_current_context = true,
   buftype_exclude = { "terminal", "nofile" },
