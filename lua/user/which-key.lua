4ae4a3a673b0d677e99f1055797c3f2fa6b447a6
G Roques <groques360@gmail.com>
2023-04-23 15:59:42 -0500

    Add group prefix names via which-key

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -1,3 +1,13 @@
-require('which-key').setup {
-  
-}
+local wk = require('which-key')
+wk.setup({})
+wk.register({
+  f = {
+    name = "find",
+  },
+  l = {
+    name = 'lsp'
+  },
+  u = {
+    name = 'ui'
+  }
+}, { prefix = "<leader>" })
