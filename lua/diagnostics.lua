70a19c9e0fff5cb1e85a74ac3179d1e82e8e8bd0
G Roques <groques360@gmail.com>
2023-04-23 15:02:08 -0500

    Override diagnostic default signs

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,3 +1,22 @@
 vim.diagnostic.config({
   virtual_text = false
 })
+-- TODO: Create icons file like LunarVim?
+-- https://github.com/LunarVim/LunarVim/blob/1.2.0/lua/lvim/icons.lua#L131-L144
+-- warning & error duplicated in statusline
+local icons = {
+  hint = "",
+  info = "",
+  warning = "",
+  error = "",
+}
+local function lspSymbol(name, icon)
+  vim.fn.sign_define(
+    'DiagnosticSign' .. name,
+    { text = icon }
+  )
+end
+lspSymbol('Error', icons.error)
+lspSymbol('Hint', icons.hint)
+lspSymbol('Info', icons.info)
+lspSymbol('Warn', icons.warning)
