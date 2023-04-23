662391b428c23ac8059546927de273caba2d4b71
G Roques <groques360@gmail.com>
2023-04-23 12:12:14 -0500

    Add treesitter movement keybinds

--- lua/user/tree-sitter.lua
+++ lua/user/tree-sitter.lua
@@ -30,7 +30,26 @@ require 'nvim-treesitter.configs'.setup {
         ["ic"] = { query = "@class.inner", desc = "inner class" },
         ["aa"] = { query = "@parameter.outer", desc = "an argument" },
         ["ia"] = { query = "@parameter.inner", desc = "inner argument" },
-        ["as"] = { query = "@scope", query_group = "locals", desc = "a scope" },
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
       },
     },
   },
