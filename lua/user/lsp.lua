83d62d1839bb1290c412469f7dc71496e5c316c3
G Roques <groques360@gmail.com>
2023-04-26 20:03:37 -0500

    Refactor diagnostics into user/lsp module

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,5 +1,6 @@
+---------------------------------------------------------------------
 -- Language Server Protocol Configuration
--- --------------------------------------
+---------------------------------------------------------------------
 local null_ls = require("null-ls")
 
 null_ls.setup({
@@ -56,3 +57,31 @@ lspconfig.lua_ls.setup {
     },
   },
 }
+
+---------------------------------------------------------------------
+-- Diagnostics
+---------------------------------------------------------------------
+vim.diagnostic.config({
+  virtual_text = false
+})
+-- TODO: Create icons file like LunarVim?
+-- https://github.com/LunarVim/LunarVim/blob/1.2.0/lua/lvim/icons.lua#L131-L144
+-- warning & error duplicated in statusline
+local icons = {
+  hint = "",
+  info = "",
+  warning = "",
+  error = "",
+}
+local function define_diagnostic_sign(name, icon)
+  local highlight = 'DiagnosticSign' .. name
+  vim.fn.sign_define(
+    highlight,
+    { text = icon, texthl = highlight }
+  )
+end
+define_diagnostic_sign('Error', icons.error)
+define_diagnostic_sign('Hint', icons.hint)
+define_diagnostic_sign('Info', icons.info)
+define_diagnostic_sign('Warn', icons.warning)
+
