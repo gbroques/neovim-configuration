e101aec04189e70ebf72601ff3c83787ef7e348c
G Roques <groques360@gmail.com>
2023-04-27 16:18:01 -0500

    Add tpope/vim-repeat
    To repeat surround operation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,10 +48,11 @@ return require('lazy').setup({
   -- Miscellaneous
   'max397574/better-escape.nvim',
   'windwp/nvim-autopairs',
-  -- TODO: Add tpope/vim-repeat or switch to a different
-  -- surround plugin like echasnovski/mini.surround or
-  -- kylechui/nvim-surround with dot repeat builtin.
+  -- TODO:
+  -- Try a new surround plugin like echasnovski/mini.surround or
+  -- kylechui/nvim-surround with dot repeat builtin?
   'tpope/vim-surround',
+  'tpope/vim-repeat',
   'folke/which-key.nvim',
   'smjonas/inc-rename.nvim',
   -- TODO:
