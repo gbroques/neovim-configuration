6da33c1037ccaedd2c98d942144374d6ac24de3c
G Roques <groques360@gmail.com>
2023-07-16 16:47:42 -0500

    Brighten unused symbols
    Add :Inspect command

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,9 @@
--- TODO: brighten unused symbols.
-require("tokyonight").setup({
+-- Troubleshooting
+-- ===============
+-- To see highlight groups, run :Inspect when cursor is over a symbol.
+-- Source:
+-- https://vi.stackexchange.com/questions/39781/how-to-get-the-highlight-group-of-the-word-under-the-cursor-in-neovim-with-trees
+require('tokyonight').setup({
   styles = {
     comments = { italic = false },
     keywords = { italic = false },
@@ -7,10 +11,13 @@ require("tokyonight").setup({
     variables = {},
   },
   on_colors = function(colors)
+    -- https://maketintsandshades.com
     colors.comment = colors.dark5
+    -- Brighten unused symbols (DiagnosticUnnecessary)
+    colors.terminal_black = colors.dark5
     colors.fg = '#d3dcf7'
-    colors.bg = "#1a1b26"
-    colors.bg_dark = "#16161e"
+    colors.bg = '#1a1b26'
+    colors.bg_dark = '#16161e'
   end,
   on_highlights = function(hl, c)
     hl.LineNr = { fg = c.fg_dark }
@@ -18,4 +25,4 @@ require("tokyonight").setup({
   end
 })
 
-vim.cmd("colorscheme tokyonight-moon")
+vim.cmd('colorscheme tokyonight-moon')
