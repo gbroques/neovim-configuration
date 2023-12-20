3e6d46e14f8cd4139de976e05745d6df7f743b6c
G Roques <groques360@gmail.com>
2023-12-20 13:42:33 -0600

    Change double-quotes to single-quotes

--- lua/user/git.lua
+++ lua/user/git.lua
@@ -1,12 +1,12 @@
-local gitsigns = require("gitsigns")
+local gitsigns = require('gitsigns')
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
@@ -21,8 +21,8 @@ gitsigns.setup {
     -- Actions
     map('n', '<leader>hs', gs.stage_hunk, { desc = 'stage hunk' })
     map('n', '<leader>hr', gs.reset_hunk, { desc = 'reset hunk' })
-    map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end)
-    map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+    map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+    map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
     map('n', '<leader>hS', gs.stage_buffer, { desc = 'stage buffer' })
     map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
     map('n', '<leader>hR', gs.reset_buffer, { desc = 'reset buffer' })
@@ -34,9 +34,9 @@ gitsigns.setup {
   end
 }
 
-local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
 local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(gitsigns.next_hunk, gitsigns.prev_hunk)
-vim.keymap.set({ "n", "x", "o" }, "]h", next_hunk_repeat, { desc = 'Next hunk' })
-vim.keymap.set({ "n", "x", "o" }, "[h", prev_hunk_repeat, { desc = 'Previous hunk' })
+vim.keymap.set({ 'n', 'x', 'o' }, ']h', next_hunk_repeat, { desc = 'Next hunk' })
+vim.keymap.set({ 'n', 'x', 'o' }, '[h', prev_hunk_repeat, { desc = 'Previous hunk' })
 
-require("scrollbar.handlers.gitsigns").setup()
+require('scrollbar.handlers.gitsigns').setup()
