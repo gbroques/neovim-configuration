daa61d238e83807b53a54c354720ac88c1e4382a
G Roques <groques360@gmail.com>
2023-12-20 18:31:13 -0600

    Improve startup time by configuring vim.g.clipboard to xsel

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,22 @@
 -- :help options
-vim.opt.clipboard = 'unnamedplus' -- allows neovim to access the system clipboard
+-- vim.opt.clipboard = 'unnamedplus' slows down startup time because Neovim has to locate the system clipboard.
+-- See the following Reddit thread:
+-- https://www.reddit.com/r/neovim/comments/17ieyn2/comment/k6tpk8a/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+-- For xsel options, see:
+-- https://github.com/neovim/neovim/blob/v0.9.4/runtime/autoload/provider/clipboard.vim#L107-L112
+vim.g.clipboard = {
+  name = 'xsel',
+  copy = {
+    ['+'] = 'xsel --nodetach -i -b',
+    ['*'] = 'xsel --nodetach -i -p',
+  },
+  paste = {
+    ['+'] = 'xsel -o -b',
+    ['*'] = 'xsel -o -p',
+  },
+  cache_enabled = 1
+}
+vim.opt.clipboard = 'unnamedplus' -- allow Neovim to access system clipboard
 vim.opt.expandtab = true          -- convert tabs to spaces in Insert mode
 vim.opt.shiftwidth = 0            -- number of spaces for each step of (auto)indent
 vim.opt.tabstop = 2               -- Number of spaces that a <Tab> in the file countsr for.
