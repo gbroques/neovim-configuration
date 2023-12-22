d3bc208cfbe37658da8f8bad45f0acd0c336cbcd
G Roques <groques360@gmail.com>
2023-12-22 13:47:03 -0600

    Move LSP keymaps from lsp to keymaps module

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -58,49 +58,57 @@ vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 vim.keymap.set('v', '<leader>p', '"_dP')
 
 -- LSP
--- https://github.com/neovim/nvim-lspconfig/blob/master/README.md#suggested-configuration
--- TODO: Should we pass in buffer number like the following example shows?
--- https://github.com/LunarVim/nvim-basic-ide/blob/3d2b182a3cffe4d3a4490fd6b8b49e8aad023c4a/lua/user/lsp.lua#L19-L48
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- LSP
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/README.md#suggested-configuration
+    -- TODO: Should we pass in buffer number like the following example shows?
+    -- https://github.com/LunarVim/nvim-basic-ide/blob/3d2b182a3cffe4d3a4490fd6b8b49e8aad023c4a/lua/user/lsp.lua#L19-L48
+    -- Use LspAttach autocommand to only map the following keys
+    -- after the language server attaches to the current buffer
 
--- Center screen after goto definition.
--- References:
--- https://www.reddit.com/r/neovim/comments/r756ur/comment/hmz7nrf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
--- https://www.reddit.com/r/neovim/comments/11frjpb/comment/jal86gg/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-local lsp_util = require('vim.lsp.util')
-local default_definition_handler = require('vim.lsp.handlers')['textDocument/definition']
-local handle_definition = function(err, result, ctx, config)
-  default_definition_handler(err, result, ctx, config)
-  vim.cmd('norm zz')
-end
-local goto_definition = function()
-  local params = lsp_util.make_position_params()
-  vim.lsp.buf_request(0, 'textDocument/definition', params, handle_definition)
-end
-vim.keymap.set('n', 'gd', goto_definition, { desc = 'Goto definition' })
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Goto declaration' })
-vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { desc = 'Goto implementation' })
-vim.keymap.set('n', 'gl', function()
-  vim.diagnostic.open_float({
-    scope = 'line',
-    header = '',
-  })
-end, { desc = 'Show line diagnostics' })
-vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { desc = 'Show signature help' })
-vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = 'Goto t(y)pe definition' })
--- TODO
--- Consider lspsaga https://nvimdev.github.io/lspsaga/codeaction/
--- which offers a preview of the code action.
--- Also consider https://github.com/aznhe21/actions-preview.nvim
-vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, { desc = 'Actions' })
-vim.keymap.set('n', '<leader>lf', function()
-  vim.lsp.buf.format { async = true }
-end, { desc = 'Format' })
-vim.keymap.set('n', '<leader>rn', function()
-  return ':IncRename ' .. vim.fn.expand('<cword>')
-end, { expr = true, desc = 'Rename' })
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover keyword' })
+    -- Center screen after goto definition.
+    -- References:
+    -- https://www.reddit.com/r/neovim/comments/r756ur/comment/hmz7nrf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    -- https://www.reddit.com/r/neovim/comments/11frjpb/comment/jal86gg/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    local lsp_util = require('vim.lsp.util')
+    local default_definition_handler = require('vim.lsp.handlers')['textDocument/definition']
+    local handle_definition = function(err, result, ctx, config)
+      default_definition_handler(err, result, ctx, config)
+      vim.cmd('norm zz')
+    end
+    local goto_definition = function()
+      local params = lsp_util.make_position_params()
+      vim.lsp.buf_request(0, 'textDocument/definition', params, handle_definition)
+    end
+    vim.keymap.set('n', 'gd', goto_definition, { buffer = ev.buf, desc = 'Goto definition' })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'Goto declaration' })
+    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = ev.buf, desc = 'Goto implementation' })
+    vim.keymap.set('n', 'gl', function()
+      vim.diagnostic.open_float({
+        scope = 'line',
+        header = '',
+      })
+    end, { buffer = ev.buf, desc = 'Show line diagnostics' })
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { buffer = ev.buf, desc = 'Show signature help' })
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { buffer = ev.buf, desc = 'Goto t(y)pe definition' })
+    -- TODO
+    -- Consider lspsaga https://nvimdev.github.io/lspsaga/codeaction/
+    -- which offers a preview of the code action.
+    -- Also consider https://github.com/aznhe21/actions-preview.nvim
+    vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'Actions' })
+    vim.keymap.set('n', '<leader>lf', function()
+      vim.lsp.buf.format { async = true }
+    end, { buffer = ev.buf, desc = 'Format' })
+    vim.keymap.set('n', '<leader>rn', function()
+      return ':IncRename ' .. vim.fn.expand('<cword>')
+    end, { buffer = ev.buf, expr = true, desc = 'Rename' })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'Hover keyword' })
+  end,
+})
 
 -- UI
 -- Inspired by AstroNvim
