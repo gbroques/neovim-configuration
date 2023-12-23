ba62bd0cc59c3ea8b971b717c9a4f526a56ca27c
G Roques <groques360@gmail.com>
2023-12-22 18:33:26 -0600

    Replace double with single quotes

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -5,42 +5,42 @@ return {
     commit = 'a2907275a6899c570d16e95b9db5fd921c167502',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local illuminate = require("illuminate")
-      vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
+      local illuminate = require('illuminate')
+      vim.g.Illuminate_ftblacklist = { 'alpha', 'NvimTree' }
       vim.api.nvim_set_keymap(
-        "n",
-        "<a-n>",
+        'n',
+        '<a-n>',
         '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>',
         { noremap = true }
       )
       vim.api.nvim_set_keymap(
-        "n",
-        "<a-p>",
+        'n',
+        '<a-p>',
         '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
         { noremap = true }
       )
 
       illuminate.configure {
         providers = {
-          "lsp",
-          "treesitter",
-          "regex",
+          'lsp',
+          'treesitter',
+          'regex',
         },
         delay = 200,
         filetypes_denylist = {
-          "dirvish",
-          "fugitive",
-          "alpha",
-          "NvimTree",
-          "packer",
-          "neogitstatus",
-          "Trouble",
-          "lir",
-          "Outline",
-          "spectre_panel",
-          "toggleterm",
-          "DressingSelect",
-          "TelescopePrompt",
+          'dirvish',
+          'fugitive',
+          'alpha',
+          'NvimTree',
+          'packer',
+          'neogitstatus',
+          'Trouble',
+          'lir',
+          'Outline',
+          'spectre_panel',
+          'toggleterm',
+          'DressingSelect',
+          'TelescopePrompt',
         },
         filetypes_allowlist = {},
         modes_denylist = {},
