5684a367ef14fdc6133307f6f1394a9d62e0637c
G Roques <groques360@gmail.com>
2023-12-26 04:03:02 -0600

    Open help window in a vertical split to the right

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -17,3 +17,13 @@
 --   vim.highlight.on_yank({ higroup = 'IncSearch' })
 -- end
 -- vim.api.nvim_create_autocmd('TextYankPost', { pattern = '*', callback = handle_highlight })
+
+-- Open help window in a vertical split to the right.
+-- https://vi.stackexchange.com/a/39697
+vim.api.nvim_create_autocmd('BufWinEnter', {
+    group = vim.api.nvim_create_augroup('help_window_right', {}),
+    pattern = { '*.txt' },
+    callback = function()
+        if vim.o.filetype == 'help' then vim.cmd.wincmd('L') end
+    end
+})
