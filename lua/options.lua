f0b413704b0cc030995bc28bc947886821835472
G Roques <groques360@gmail.com>
2023-12-28 19:02:57 -0600

    Set fillchars diff to space instead of dash

--- lua/options.lua
+++ lua/options.lua
@@ -34,3 +34,4 @@ vim.opt.signcolumn = 'yes'        -- always show sign column to avoid layout shi
 vim.opt.ruler = false             -- Don't show the line and column number of the cursor position
 vim.opt.laststatus = 3            -- Enable 'global' statusline; https://www.youtube.com/watch?v=jH5PNvJIa6o
 vim.opt.splitright = true         -- Put new buffer on the right for vertical splits
+vim.opt.fillchars:append { diff = " " }
