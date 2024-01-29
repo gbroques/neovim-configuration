4499c17182f769c4f37725153fffaec808c76dfe
G Roques <groques360@gmail.com>
2024-01-28 20:23:38 -0600

    Install tree-sitter parsers via git

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -33,6 +33,7 @@ return {
       local function ends_with(str, ending)
         return ending == '' or str:sub(- #ending) == ending
       end
+      -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3232#issuecomment-1198882715
       require('nvim-treesitter.install').prefer_git = true
       require('nvim-treesitter.configs').setup({
         highlight = { enable = true },
