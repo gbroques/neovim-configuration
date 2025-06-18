096a26172143144c9d4402d3851e327ffd034447
G Roques <c-g.roques@charter.com>
2025-06-18 13:04:01 -0500

    Update nvim-spectre

--- lua/plugins/global-find-and-replace.lua
+++ lua/plugins/global-find-and-replace.lua
@@ -2,7 +2,7 @@
 return {
   {
     'nvim-pack/nvim-spectre',
-    commit = 'f4dc98ec45ecded2344aa3aac2d7cc43ad236858',
+    commit = '72f56f7585903cd7bf92c665351aa585e150af0f',
     cmd = 'Spectre',
     config = true
   }
