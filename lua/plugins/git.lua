6810bada7485841757fc401f1e1f2ce4a2756364
G Roques <c-g.roques@charter.com>
2025-07-08 13:31:06 -0500

    Update git sign for delete

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,8 +14,8 @@ return {
         signs = {
           add = { text = '▎' },
           change = { text = '▎' },
-          delete = { text = '契' },
-          topdelete = { text = '契' },
+          delete = { text = '󰐊' },
+          topdelete = { text = '󰐊' },
           changedelete = { text = '▎' },
         },
         attach_to_untracked = false,
