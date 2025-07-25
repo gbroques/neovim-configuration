1f4735f65321037b93fcae87541479336eb775d6
G Roques <groques360@gmail.com>
2025-07-24 22:06:35 -0500

    Add TODO comments for Neovim 0.10 native comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,9 @@
 return {
   {
+    -- TODO: Some commenting behavior is built-in to Neovim since 0.10
+    -- See https://gpanders.com/blog/whats-new-in-neovim-0.10/#builtin-commenting
+    -- See Is this still needed with Neovim 0.10?
+    --     https://github.com/numToStr/Comment.nvim/issues/453
     'numToStr/Comment.nvim',
     commit = 'e30b7f2008e52442154b66f7c519bfd2f1e32acb',
     event = { 'BufReadPre', 'BufNewFile' },
@@ -12,6 +16,8 @@ return {
       {
         'JoosepAlviste/nvim-ts-context-commentstring',
         commit = '9c74db656c3d0b1c4392fc89a016b1910539e7c0',
+        -- TODO: Make this work with Neovim native comments?
+        -- See https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#native-commenting-in-neovim-010
         config = function()
           require('ts_context_commentstring').setup {
             enable_autocmd = false
