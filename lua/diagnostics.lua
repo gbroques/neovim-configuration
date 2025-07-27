44c2b69f5565c00809150cf763131d62a65db2a8
G Roques <groques360@gmail.com>
2025-07-26 21:11:38 -0500

    Add TODO for trouble.nvim

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -2,6 +2,7 @@
 -- Diagnostics
 ---------------------------------------------------------------------
 local icons = require('icons')
+-- TODO: Consider https://github.com/folke/trouble.nvim to view diagnostics for LSP workspace
 vim.diagnostic.config({
   virtual_text = { current_line = true },
   signs = {
