7c8aa2360e207cfa7d0361a008be4928e30398c5
G Roques <groques360@gmail.com>
2025-07-26 20:42:06 -0500

    Set vim.opt.splitbelow = true

--- lua/options.lua
+++ lua/options.lua
@@ -74,6 +74,7 @@ vim.opt.signcolumn = 'yes'        -- always show sign column to avoid layout shi
 vim.opt.ruler = false             -- Don't show the line and column number of the cursor position
 vim.opt.laststatus = 3            -- Enable 'global' statusline; https://www.youtube.com/watch?v=jH5PNvJIa6o
 vim.opt.splitright = true         -- Put new buffer on the right for vertical splits
+vim.opt.splitbelow = true         -- Put new buffer on the bottom for horizontal splits
 vim.opt.fillchars:append { diff = " " }
 vim.opt.guifont = 'JetBrainsMono NF'
 if vim.fn.has('macunix') == 0 then
